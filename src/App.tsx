237f19c49c15d5071a6ca1b70df34c000331d4c3
mxrsel <marsel14082006@gmail.com>
2024-06-14 13:27:31 +0600

    finish app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import MovieForm from './components /MovieForm';
+import MovieList from './components /MovieList';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Movie {
+  id: number;
+  title: string;
 }
 
-export default App
+const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAddMovie = (newMovie: string) => {
+    const newMovieItem: Movie = {
+      id: Math.floor(Math.random() * 1000), // Генерируем случайный id
+      title: newMovie,
+    };
+    setMovies((prevMovies) => [...prevMovies, newMovieItem]);
+  };
+
+  const handleEditMovie = (id: number, title: string) => {
+    setMovies((prevMovies) =>
+      prevMovies.map((movie) =>
+        movie.id === id ? { ...movie, title: title } : movie
+      )
+    );
+  };
+
+  const handleRemoveMovie = (id: number) => {
+    setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+  };
+
+  return (
+    <div>
+      <MovieForm onAddMovie={handleAddMovie} />
+      <MovieList
+        movies={movies.map((movie, index) => ({ ...movie, id: index + 1 }))}
+        onEditMovie={handleEditMovie}
+        onRemoveMovie={handleRemoveMovie}
+      />
+    </div>
+  );
+};
+
+export default App;
